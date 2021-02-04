974a25367909cb9dcd75f7e4485f1a2aab4af22b
khunanan <khunanan.pan@gmail.com>
2021-02-04 10:04:46 +0700

    edit package name test01

--- Open_newApp/Open_newApp/Open_newApp/MainPage.xaml.cs
+++ Open_newApp/Open_newApp/Open_newApp/MainPage.xaml.cs
@@ -19,7 +19,7 @@ namespace Open_newApp
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            var appname = @"id568388474";
+            var appname = @"568388474";
             var result = await DependencyService.Get<IAppHandler>().LaunchApp(appname);
         }
 
